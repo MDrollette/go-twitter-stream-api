5694204d524f68516c686a5490483dfc937005dc
Aaron Groves <aarongroves@gmail.com>
2013-11-04 05:35:52 +1000

    go format and added stop channel

--- twitter_api.go
+++ twitter_api.go
@@ -1,138 +1,156 @@
 package twitter_api
 
 import (
-  "github.com/NOX73/go-oauth"
-  "net/http"
-  "bufio"
-  "errors"
-  "encoding/json"
+	"bufio"
+	"encoding/json"
+	"errors"
+	"github.com/NOX73/go-oauth"
+	"net/http"
 )
 
 const (
-  NewRequestMethod = "POST"
-  NewRequestURL = "https://stream.twitter.com/1.1/statuses/filter.json"
-  TwitterStreamApiConnestionError = "Error: Response status code not 200."
+	NewRequestMethod                = "POST"
+	NewRequestURL                   = "https://stream.twitter.com/1.1/statuses/filter.json"
+	TwitterStreamApiConnestionError = "Error: Response status code not 200."
 )
 
 type Credentials struct {
-  OauthConsumerKey string
-  OauthToken string
-  OauthConsumerSecret string
-  OauthTokenSecret string
+	OauthConsumerKey    string
+	OauthToken          string
+	OauthConsumerSecret string
+	OauthTokenSecret    string
 }
 
 type Message struct {
-  Error error
-  Response *http.Response
-  Tweet *Tweet
+	Error    error
+	Response *http.Response
+	Tweet    *Tweet
 }
 
 // >> Tweet
 // stream api message
 
 type TweetJSON struct {
-  Text string
-  User struct {
-    Id int
-    Screen_name string
-    Name string
-    Description string
-    Profile_image_url_https string
-  }
+	Text string
+	User struct {
+		Id                      int
+		Screen_name             string
+		Name                    string
+		Description             string
+		Profile_image_url_https string
+	}
 }
 
 type Tweet struct {
-  Body string
-  JSON *TweetJSON
+	Body string
+	JSON *TweetJSON
 }
 
 func (t *Tweet) Text() string {
-  if t.JSON == nil{ t.ParseJSON() }
-  return t.JSON.Text
+	if t.JSON == nil {
+		t.ParseJSON()
+	}
+	return t.JSON.Text
 }
 
 func (t *Tweet) UserID() int {
-  if t.JSON == nil{ t.ParseJSON() }
-  return t.JSON.User.Id
+	if t.JSON == nil {
+		t.ParseJSON()
+	}
+	return t.JSON.User.Id
 }
 
 func (t *Tweet) UserName() string {
-  if t.JSON == nil{ t.ParseJSON() }
-  return t.JSON.User.Screen_name
+	if t.JSON == nil {
+		t.ParseJSON()
+	}
+	return t.JSON.User.Screen_name
 }
 
 func (t *Tweet) ParseJSON() {
-  t.JSON = &TweetJSON{}
-  _ = json.Unmarshal([]byte(t.Body), t.JSON) 
+	t.JSON = &TweetJSON{}
+	_ = json.Unmarshal([]byte(t.Body), t.JSON)
 }
 
 // << Tweet
 
-
 func NewCredentials(consumer_key, token, consumer_secret, token_secret string) *Credentials {
-  return &Credentials{consumer_key, token, consumer_secret, token_secret}
+	return &Credentials{consumer_key, token, consumer_secret, token_secret}
 }
 
+func TwitterStream(ch chan Message, stop chan bool, credentials *Credentials, params map[string]string) {
+	var message Message
 
-func TwitterStream (ch chan Message, credentials *Credentials, params map[string]string){
-  var message Message
+	c := oauth.NewCredentials(credentials.OauthConsumerKey, credentials.OauthToken, credentials.OauthConsumerSecret, credentials.OauthTokenSecret)
 
-  c := oauth.NewCredentials(credentials.OauthConsumerKey, credentials.OauthToken, credentials.OauthConsumerSecret, credentials.OauthTokenSecret)
+	r, _ := oauth.NewRequest(NewRequestMethod, NewRequestURL, params, c)
 
-  r, _ := oauth.NewRequest(NewRequestMethod, NewRequestURL, params, c)
+	client := http.Client{}
+	resp, err := client.Do(r.HttpRequest())
 
-  client := http.Client{}
-  resp, err := client.Do(r.HttpRequest())
+	if err == nil {
+		err = CheckError(resp)
+	}
 
-  if err == nil{
-    err = CheckError(resp)
-  }
+	if err != nil {
+		message = Message{
+			Error:    err,
+			Response: resp,
+		}
 
-  if err != nil {
-    message = Message{
-      Error:err,
-      Response: resp,
-    }
+		ch <- message
+		return
+	}
 
-    ch <- message
-    return
-  }
+	body_reader := bufio.NewReader(resp.Body)
 
-  body_reader := bufio.NewReader(resp.Body)
+	for {
+		var part []byte //Part of line
+		var prefix bool //Flag. Readln readed only part of line.
 
-  for {
-    var part []byte //Part of line
-    var prefix bool //Flag. Readln readed only part of line.
+		part, prefix, err := body_reader.ReadLine()
+		if err != nil {
+			break
+		}
 
-    part, prefix, err := body_reader.ReadLine()
-    if err != nil { break }
+		if len(part) == 0 {
+			continue
+		}
 
-    if len(part) == 0 { continue }
+		buffer := append([]byte(nil), part...)
 
-    buffer := append([]byte(nil), part...)
+		for prefix && err == nil {
+			part, prefix, err = body_reader.ReadLine()
+			buffer = append(buffer, part...)
+		}
+		if err != nil {
+			break
+		}
 
-    for prefix && err == nil {
-      part, prefix, err = body_reader.ReadLine()
-      buffer = append(buffer, part...)
-    }
-    if err != nil { break }
+		tweet := &Tweet{
+			Body: string(buffer),
+		}
 
-    tweet := &Tweet{
-      Body: string(buffer),
-    }
+		message = Message{
+			Response: resp,
+			Tweet:    tweet,
+		}
 
-    message = Message{
-      Response: resp,
-      Tweet: tweet,
-    }
+		select {
+		case s := <-stop:
+			if s {
+				return
+			}
+		default:
+		}
 
-    ch <- message
-  }
+		ch <- message
+	}
 }
 
 func CheckError(r *http.Response) error {
-  if r.StatusCode != 200{
-    return errors.New(TwitterStreamApiConnestionError)
-  }
-  return nil
+	if r.StatusCode != 200 {
+		return errors.New(TwitterStreamApiConnestionError)
+	}
+	return nil
 }
--- twitter_api_test.go
+++ twitter_api_test.go
@@ -1,32 +1,34 @@
 package twitter_api
 
 import (
-  . "launchpad.net/gocheck"
-  "testing"
+	. "launchpad.net/gocheck"
+	"testing"
 )
 
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { TestingT(t) }
+
 type OAuthSuite struct{}
+
 var _ = Suite(&OAuthSuite{})
 
 func (s *OAuthSuite) TestCreateClient(c *C) {
 
-  ch := make(chan Message)
-  params := make(map[string]string, 1)
+	ch := make(chan Message)
+	params := make(map[string]string, 1)
 
-  credentials := NewCredentials("XjY7q0CYwRxSBzCpUeRDzQ", "214373359-jn77FNlrKEajR4Gpp9l5msb1KXCGXZ7QeJPtt5TF", "cuseCPmxY4taUEmouOhXIvR7MVSUWdRKjKHvHKgVvOk", "tO5hW1ye3myBnT78DspVbTKWFgadvKeU1EOiV3o5Tg")
+	credentials := NewCredentials("XjY7q0CYwRxSBzCpUeRDzQ", "214373359-jn77FNlrKEajR4Gpp9l5msb1KXCGXZ7QeJPtt5TF", "cuseCPmxY4taUEmouOhXIvR7MVSUWdRKjKHvHKgVvOk", "tO5hW1ye3myBnT78DspVbTKWFgadvKeU1EOiV3o5Tg")
 
-  params["track"] = "twitter"
+	params["track"] = "twitter"
 
-  go TwitterStream(ch, credentials, params)
+	go TwitterStream(ch, credentials, params)
 
-  message := <- ch
+	message := <-ch
 
-  c.Assert(message.Response.StatusCode, Equals, 200)
-  c.Assert(message.Tweet, NotNil)
-  c.Assert(message.Tweet.Body, NotNil)
-  c.Assert(message.Tweet.Text(), NotNil)
-  c.Assert(message.Tweet.UserID(), NotNil)
-  c.Assert(message.Tweet.UserName(), NotNil)
+	c.Assert(message.Response.StatusCode, Equals, 200)
+	c.Assert(message.Tweet, NotNil)
+	c.Assert(message.Tweet.Body, NotNil)
+	c.Assert(message.Tweet.Text(), NotNil)
+	c.Assert(message.Tweet.UserID(), NotNil)
+	c.Assert(message.Tweet.UserName(), NotNil)
 }
